fd71e7e4d38ab6ae95eada7a85b9bfdae087b72f
Bruno Vieira Costa <brunopsycon@gmail.com>
2015-08-02 12:37:10 -0300

    meu primeiro commit na página

---
+++ script.js
@@ -0,0 +1,59 @@
+
+			$(function(){
+				$('section').hide();
+				$("#main-games").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-games").css("text-decoration", "underline");
+					$('section').hide();
+					$("#games").show();
+				})
+				$("#main-programming").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-programming").css("text-decoration", "underline");
+					$('section').hide();
+					$("#programming").show();
+				})
+				$("#main-foundations").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-foundations").css("text-decoration", "underline");
+					$('section').hide();
+					$("#foundations").show();
+				})
+				$("#main-websites").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-websites").css("text-decoration", "underline");
+					$('section').hide();
+					$("#websites").show();
+				})
+				$("#main-comics").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-comics").css("text-decoration", "underline");
+					$('section').hide();
+					$("#comics").show();
+				})
+				$("#main-paintings").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-paintings").css("text-decoration", "underline");
+					$('section').hide();
+					$("#paintings").show();
+				})
+				$("#main-tutorials").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-tutorials").css("text-decoration", "underline");
+					$('section').hide();
+					$("#tutorials").show();
+				})
+				$("#main-prizes").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-prizes").css("text-decoration", "underline");
+					$('section').hide();
+					$("#prizes").show();
+				})
+				$("#main-musics").mouseover(function() {
+					$('#main li').css("text-decoration", "none");
+					$("#main-musics").css("text-decoration", "underline");
+					$('section').hide();
+					$("#musics").show();
+				})
+			});
+		
